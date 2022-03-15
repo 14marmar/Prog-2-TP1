131d68ed827c82575d5efaa2749254e853a56720
14marmar <91103200+14marmar@users.noreply.github.com>
2022-03-14 20:50:36 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main( String[] args ) {
+    // Votre code ici
+    System.out.println( "Hello, " + args[0] + " !" );
+    }
+    }
---
+++ test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String args[]){
+          //This line will print "Hello World".
+          System.out.println("Hello World.");
+    }
+}   
