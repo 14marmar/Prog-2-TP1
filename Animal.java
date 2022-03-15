93b2a3bb88067aa7b2aa09117ec4b9b1c9911eb5
AlexandreStang <lealexandrestang@gmail.com>
2022-03-15 19:55:26 -0400

    Initialization of Animal.java

--- Animal.java
+++ Animal.java
@@ -15,3 +15,117 @@
 **/
 
     // TO BE COMPLETED //
+
+public class Animal implements ProiePredateur {
+
+    protected int age, ageMax, ageMature;
+    protected double masse, facCroissance;
+    protected boolean estPredateur, estProie, estVivant;
+
+
+    @Override
+    public void naitre() {
+        // TODO Auto-generated method stub
+        age = 0;
+        masse = 10;
+    }
+
+    @Override
+    public void vieillir() {
+        // TODO Auto-generated method stub
+        age++;
+        masse = masse * facCroissance;
+
+        if (age > ageMax) {
+            mourir();
+        }
+    }
+
+    @Override
+    public void manger() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Animal accoucher() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void mourir() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean estVivant() {
+        return estVivant;
+    }
+
+    @Override
+    public boolean estMature() {
+        
+        if (age >= ageMature) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    @Override
+    public void setProie(boolean proie) {
+        this.estProie = proie;
+    }
+
+    @Override
+    public boolean estProie() {
+        return estProie;
+    }
+
+    @Override
+    public void setPredateur(boolean predateur) {
+        this.estPredateur = predateur;
+    }
+
+    @Override
+    public boolean estPredateur() {
+        return estPredateur;
+    }
+
+    @Override
+    public double getMasse() {
+        return masse;
+    }
+
+    @Override
+    public void setMasse(double masse) {
+        this.masse = masse;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int getAgeMax() {
+        return ageMax;
+    }
+
+    @Override
+    public int getAgeMature() {
+        return ageMature;
+    }
+
+
+
+
+}
